fed292d749c7d153b79ed20f3a1c4d6ed6cee146
Kuba Janoszek <kuba.janoszek@gmail.com>
2012-08-15 22:45:44 +0200

    Changing mode of manage.py for django_project template added.

--- django_project/config.py
+++ django_project/config.py
@@ -1,9 +1,18 @@
-from boilerplate import Configuration
+# -*- coding: utf-8 -*-
+import subprocess
 from random import choice
+
+from boilerplate import Configuration, ProjectCreator as PC
+
+
+class ProjectCreator(PC):
+    def after_file_create(self, destination_file_path):
+        if destination_file_path.endswith('manage.py'):
+            subprocess.call(['chmod', '+x', destination_file_path])
 
 
 conf = Configuration(__file__, {
     'secret_key': ''.join([
         choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)
     ]),
-})
+}, creator_class=ProjectCreator)
